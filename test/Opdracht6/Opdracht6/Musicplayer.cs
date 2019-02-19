426cc86841cac5f773ffd188c1ac08447d893125
jeffreyPieters <45628912+jeffreyPieters@users.noreply.github.com>
2019-02-19 08:51:19 +0100

    test programma

---
+++ test/Opdracht6/Opdracht6/Musicplayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht6
+{
+    class Musicplayer
+    {
+        public Musicplayer()
+        {
+
+        }
+
+        public void AddArtist(Artist artist)
+        {
+
+        }
+
+        public void AddSong(Song song)
+        {
+
+        }
+
+        public void AddPlaylist(Playlist playlist)
+        {
+
+        }
+
+        public void RemovePlaylist(Playlist playlist)
+        {
+
+        }
+
+        public void PlaySong(Song song)
+        {
+
+        }
+
+        public void PlayPlaylist(Playlist playlist)
+        {
+
+        }
+
+        public Song IsPlaying()
+        {
+
+        }
+
+        public void StopPlaying()
+        {
+
+        }
+    }
+}
